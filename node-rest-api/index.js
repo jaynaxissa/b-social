6ea3c3b85ceaf2fae287557f389ad22480263cb2
jaynaxissa <jaynaxissa@gmail.com>
2023-02-03 12:56:20 +0700

    first api commit

---
+++ node-rest-api/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+dotenv.config();
+
+mongoose.set("strictQuery", false); // to make the warning go away
+
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true }, () => {
+  console.log("Connected to MongoDB");
+});
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(6600, () => {
+  console.log("Backend server is running:)");
+});
